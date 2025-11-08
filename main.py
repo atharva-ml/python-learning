d93247a9a6b22f60f07cd71e04b3fb8017bcc0ce
atharva-ml <atharvabhosale1487@gmail.com>
2025-11-08 18:20:32 +0530

    Create main.py
    Added basic variable practice file

---
+++ main.py
@@ -0,0 +1,13 @@
+#Variables :- container of value, it behaves as it was a value it contains
+#Below given are string :- strings are basically series of characters
+first_name = "ATHARVA"
+second_name = "Khushi"
+email = "atharvabhosale1487@gmail.com"
+password = "Khushu@123"
+food = "Burger"
+
+print(f"I love you {second_name}")
+print(f"Your email is: {email}")
+print(f"Your password is: {password}")
+print(f"Your favourite food is: {food}")
+
