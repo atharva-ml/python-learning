9e9274cfab99485cc559931f2e4da426e86a2115
atharva-ml <atharvabhosale1487@gmail.com>
2025-11-08 18:24:35 +0530

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #Variables :- container of value, it behaves as it was a value it contains
 #Below given are string :- strings are basically series of characters
 first_name = "ATHARVA"
-second_name = "Khushi"
-email = "atharvabhosale1487@gmail.com"
-password = "Khushu@123"
+second_name = "Brother"
+email = "atharvabhosale14@gmail.com"
+password = "Fake@123"
 food = "Burger"
 
 print(f"I love you {second_name}")
